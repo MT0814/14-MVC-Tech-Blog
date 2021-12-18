69c132c8cfe6778515beb2a4d3a2ec4100f62326
MT0814 <millie_tsai@me.com>
2021-12-17 22:45:51 -0800

    comment.js works, removed commentRoutes,failed to fetch the comment form

--- public/js/comment-btn.js
+++ public/js/comment-btn.js
@@ -1,17 +1,27 @@
 async function newFormHandler(event) {
     event.preventDefault();
+    console.log("inside")
+    const content = document.querySelector('#content').value.trim();
 
     const response = await fetch('/api/comment/:id', {
+
         method: 'POST',
         body: JSON.stringify({
-            title,
-            content,
-            image
+            content
         }),
         headers: {
             'Content-Type': 'application/json',
         },
+
     });
+    console.log(response)
+    //if the comment is added, the 'all' template will be rerendered
+    if (response.ok) {
+        document.location.replace('/comment/:id');
+    } else {
+        alert('Failed to add comment');
+    }
+
 
 }
 
